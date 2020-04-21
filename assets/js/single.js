9ad8b85fb4c57fcba19c695ba382d6c9ab48d9cf
Kaynalem <61393721+Kaynalem@users.noreply.github.com>
2020-04-21 18:06:59 -0500

    link pages and add error handling

--- assets/js/single.js
+++ assets/js/single.js
@@ -1,11 +1,27 @@
 var issueContainerEl = document.querySelector("#issues-container");
 var limitWarningEl = document.querySelector("#limit-warning");
+var repoNameEl = document.querySelector("#repo-name");
 
+var getRepoName = function() {
+    // grab repo name from url query string
+    var queryString = document.location.search;
+    var repoName = queryString.split("=")[1];
+
+    if(repoName) {
+        // display repo name on the page
+        repoNameEl.textContent = repoName;
+
+        getRepoIssues(repoName);
+    } else {
+        // if no repo was given, redirect to the homepage
+        document.location.replace("./index.html");
+    }
+}
 var getRepoIssues = function(repo) {
     var apiUrl = "https://api.github.com/repos/" + repo + "/issues?direction=asc";
 
     fetch(apiUrl).then(function(response) {
-        //request was successful
+        // request was successful
         if (response.ok) {
             response.json().then(function(data) {
                 // pass response data to dom function
@@ -18,7 +34,8 @@ var getRepoIssues = function(repo) {
             });
         }
         else {
-            alert("There was a problem with your request!");
+        // if not successful, redirect to homepage
+        document.location.replace("./index.html");
         }
     });
 };
@@ -72,4 +89,5 @@ var displayWarning = function(repo) {
     // append to warning container
     limitWarningEl.appendChild(linkEl);
 };
-getRepoIssues();
+
+getRepoName();
